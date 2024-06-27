8f5997fd242d3ca8201364fb36284d543d9187fc
Pamelita123 <pame.ado3780@gmail.com>
2024-06-27 11:34:27 -0600

    implementacion de un register y proceso de login

--- src/server.mjs
+++ src/server.mjs
@@ -14,11 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Ruta para el formulario de registro
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'));
+    res.sendFile(path.join(__dirname, '../public/html/index.html'));
 });
 
 // Ruta para el registro de usuarios
