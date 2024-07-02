281bc69db26acd106b8d2f6a81d977e4e617599c
sweetArizCider <arizah.applecider+github@gmail.com>
2024-07-01 19:59:20 -0600

    index con register y productos :)

--- src/server.mjs
+++ src/server.mjs
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static(path.join(__dirname, '../../GLASS_STORE')));
 
 
 // Ruta para el formulario de registro
