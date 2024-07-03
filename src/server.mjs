686f51702c412132d6bd6560e6a5e385d19b63fa
Thaniath <thaniasofiamedina35@gmail.com>
2024-07-03 14:54:04 -0600

    solo cambie y vinvule las rutas en el server

--- src/server.mjs
+++ src/server.mjs
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '../../GLASS_STORE')));
 
 // Ruta para el formulario de registro
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/html/index.html'));
+    res.sendFile(path.join(__dirname, '../src/views/administrador/viewrecibos.html'));
 });
 
 // Ruta para el registro de usuarios
